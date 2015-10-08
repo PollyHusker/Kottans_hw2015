7113a83fc2c7f6bcf5506a378c814d1ec70505af
PollyHusker <baidenko.polina@gmail.com>
2015-10-09 00:06:08 +0300

    added CeasarCipher [+] classlib

---
+++ CeasarCipher/Ceasar.Lib/CeasarCipher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasar.Lib
+{
+    public class CeasarCipher
+    {
+        private int Offset;
+        public CeasarCipher(int offset)
+        {
+            Offset = offset;
+        }
+
+        public string Encrypt(string data)
+        {
+            return string.Empty;
+        }
+
+        public string Decrypt(string data)
+        {
+            return string.Empty;
+        }
+    }
+}
