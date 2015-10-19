139b8904c24f99e8b6e56fa85bc03cf540936beb
PollyHusker <baidenko.polina@gmail.com>
2015-10-20 01:32:17 +0300

    [+]classLibrary Matrix; [+]classes Matrix Size. Not impl

---
+++ Matrix/Matrix/Matrix.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class CoolMatrix
+    {
+        public Size Size { get; set; }
+        private int[,] _array;
+
+        public CoolMatrix(int[,] array)
+        {
+            Array.Copy(array, _array, array.Length);
+        }
+    }
+}
---
+++ Matrix/Matrix/Size.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Size
+    {
+        private int height;
+        private int width;
+
+        public Size(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
