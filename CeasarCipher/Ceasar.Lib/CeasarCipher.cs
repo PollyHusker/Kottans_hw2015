42677c136a39ffb0477c74ad4248073bbc3979a7
PollyHusker <baidenko.polina@gmail.com>
2015-10-20 01:07:17 +0300

    Closed #1

--- CeasarCipher/Ceasar.Lib/CeasarCipher.cs
+++ CeasarCipher/Ceasar.Lib/CeasarCipher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Net.Security;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Ceasar.Lib
     public class CeasarCipher
     {
         private int Offset;
+
         public CeasarCipher(int offset)
         {
             Offset = offset;
@@ -17,12 +19,49 @@ namespace Ceasar.Lib
 
         public string Encrypt(string data)
         {
-            return string.Empty;
+            if (data == null)
+                throw new ArgumentNullException();
+            if (data.Equals(String.Empty))
+                return string.Empty;
+            CheckStringForNonSymbolic(data);
+
+            var rez = new char[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == ' ')
+                    rez[i] = ' ';
+                else
+                    rez[i] = (char) ((((((int) data[i] - 33) + Offset)%94) + 94)%94 + 33);
+            }
+            return new string(rez);
         }
 
         public string Decrypt(string data)
         {
-            return string.Empty;
+            if (data == null)
+                throw new ArgumentNullException();
+            if (data.Equals(String.Empty))
+                return string.Empty;
+            CheckStringForNonSymbolic(data);
+
+            var rez = new char[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == ' ')
+                    rez[i] = ' ';
+                else
+                    rez[i] = (char) ((((((int) data[i] - 33) - Offset)%94) + 94)%94 + 33);
+            }
+            return new string(rez);
+        }
+
+        private void CheckStringForNonSymbolic(string data)
+        {
+            foreach (char ch in data)
+            {
+                if ((((int) ch >= 0) && ((int) ch <= 31)) || ((int) ch == 127))
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
